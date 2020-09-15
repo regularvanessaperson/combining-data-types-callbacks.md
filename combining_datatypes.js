9ff8a3edb773114f3c1b733c75fda9cdf50c3efc
regularvanessaperson <vamavila4@gmail.com>
2020-09-14 20:36:37 -0500

    Completed vending machine, callbacks and error reading

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -69,3 +69,79 @@ const powerButton = () => {
 }
 
 powerButton()()
+
+//Model a Vending Machine 
+
+const vendingMachine = {
+   vend: (index) => {
+        let snacks = [{name: "Hot Cheetos", price: "1,00"},
+        {name:"M&Ms",price: "1.00"},
+        {name: "PayDay", price: "1.00"}
+        ]
+        return snacks[index]}
+    }
+
+
+console.log(vendingMachine.vend(1))
+
+// Callbacks
+
+//1. Make a function add that takes two arguments (numbers) and sums them together
+const sumOf = (a,b) => {
+    return a+b
+  }
+
+sumOf(1,1)
+
+//2. Make a function subtract that takes two arguments (numbers) and subtracts them
+const subtractNum = (a,b) => {
+    return a-b
+   }
+   
+   subtractNum(1,1)
+
+//3. Make a function multiply that takes two arguments and multiplies them
+const multiplyNum = (a,b) => {
+    return a*b
+   }
+   
+   multiplyNum(1,1)
+//4. Make a function divide that takes two arguments and divides them
+const divideNum = (a,b) => {
+    return a/b
+   }
+   
+   divideNum(2,4)
+//5. Make a function calculate that takes three arguments. Assume the two arguments are a number ie num1, num2 and a function called operates (a callback).
+const calculate = (operates,a,b) =>{
+    operates(a,b)
+}
+
+//6. Make it so that when calculate is invoked, the callback "operates" on the numbers and returns the value.
+const calculate = (operates,a,b) =>{
+    return operates(a,b)
+}
+calculate()
+//7. Call calculate 4 times, each time using one of the operation functions you wrote
+
+const calculate = (operates,a,b) =>{
+    return operates(a,b)
+}
+calculate(divideNum,3,6)
+calculate(multiplyNum,4,3)
+calculate(sumOf,9,7)
+calculate(subtractNum,8,3)
+
+//Function definition placement
+
+const bar = () => {
+    console.log('bar here');
+}
+bar();
+
+const foo = () => {
+    console.log('foo here');
+}
+foo();
+
+// the error produced by calling the function above the actual function says that it cannot access the function before initialization. It cannot call a function before it the function is read.
